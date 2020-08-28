5724bd90f1ce95a4a2876a6a21e09cba9df8ee4f
tanakaryo <ch5cxcxxh@gmail.com>
2020-08-28 12:10:30 +0900

    edit files

--- chap02/scripts_es5/dest_obj2.js
+++ chap02/scripts_es5/dest_obj2.js
@@ -1,3 +1,8 @@
 'use strict';
 
+let book = {title: "Javaポケットリファレンス", publish: '技術評論社', price: 2680, other:{keyword: "Java SE 8",logo: 'logo.jpd'}};
+let {title,other, other:{keyword}} =book;
 
+console.log(title);
+console.log(other);
+console.log(keyword);
--- chap02/scripts_es5/dest_obj3.js
+++ chap02/scripts_es5/dest_obj3.js
@@ -1,2 +1,7 @@
 'use strict';
 
+let book = {title: 'Javaポケットリファレンス', publish: '技術評論社'};
+let {title: name, publish: company} = book;
+
+console.log(name);
+console.log(company);
